7a61fee7d894711eaa1086103e501635d82e0d1b
Tatiana <kazantsevat@gmail.com>
2016-09-10 15:16:27 -0700

    karma

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,35 @@
-/**
- * Created by mainhackintosh on 9/10/16.
- */
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
-gulp.task('serve', function() {
+gulp.task('serve',['server'],function(){
    browserSync.init({
-      notify: false,
-      port: 8080,
-      server: {
-         baseDir: ['app'],
-         routes: {
-            '/bower-components': 'bower-components'
+      notify:false,
+      port:8080,
+      server:{
+         baseDir:["app"],
+         routes:{
+            '/bower_components':'bower_components'
          }
       }
    });
 
-   gulp.watch(['app/**/*.*']).on('change', browserSync.reload);
+   gulp.watch(['app/**/*.*'])
+      .on('change',browserSync.reload);
+});
+
+
+gulp.task('serve-test',function(){
+   browserSync.init({
+      notify:false,
+      port:8081,
+      server:{
+         baseDir:["test","app"],
+         routes:{
+            '/bower_components':'bower_components'
+         }
+      }
+   });
+
+   gulp.watch(['app/**/*.*'])
+      .on('change',browserSync.reload);
 });
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,3 +1,8 @@
 /**
  * Created by mainhackintosh on 9/10/16.
  */
+describe('The address book app', function() {
+   it('should work', function() {
+      chai.assert.isArray([]);
+   });
+});
